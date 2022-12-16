a1e9eb98f40bf6bdef4bf83877a17d4af06c16d1
helgeskjellevik <helge.skjellevik@gmail.com>
2022-12-16 11:15:01 +0100

    fix: date format on enrollment note after successful sms notification

--- ks_patches/create_notification_for_sms.js
+++ ks_patches/create_notification_for_sms.js
@@ -1,14 +1,20 @@
+/* global angular */
+
 export function createNotificationForSms(smsMsg, phoneNumber, dateUtils, currentSelection, sessionStorageService, enrollmentService) {
     var selections = currentSelection.get();
     var enrollment = selections.selectedEnrollment && selections.selectedEnrollment.enrollment;
     if (enrollment) {
         var selectedEnrollment = selections.selectedEnrollment;
 
+        var copiedEnrollment = angular.copy(selectedEnrollment);
+        copiedEnrollment.incidentDate = dateUtils.formatFromUserToApi(copiedEnrollment.incidentDate);
+        copiedEnrollment.enrollmentDate = dateUtils.formatFromUserToApi(copiedEnrollment.enrollmentDate);
+
         var noteText = `SMS sendt til ${phoneNumber}: ${smsMsg}`;
         var newNote = {
             value: noteText,
         };
-        selectedEnrollment.notes = [newNote];
-        enrollmentService.updateForNote(selectedEnrollment);
+        copiedEnrollment.notes = [newNote];
+        enrollmentService.updateForNote(copiedEnrollment);
     }
 }
